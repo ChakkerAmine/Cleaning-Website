93529ca47d8309ae39edd99839231991e009b730
ChakkerAmine <chakkeramine18@gmail.com>
2022-07-17 17:27:48 +0100

    Displayin Posts by Condition

--- wp-content/themes/learning_wordpress/index.php
+++ wp-content/themes/learning_wordpress/index.php
@@ -1,7 +1,17 @@
 <?php 
 
 if(have_posts()){
-    echo "Kayn des posts";
+    
+    while(have_posts()){
+        the_post();
+        ?>
+        <h1><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
+        <hr>
+        <?php
+    }
+
+
 }else{
     echo "Makayn hta post";
 }
