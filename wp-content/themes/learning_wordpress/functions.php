66db711fc97f22ddc4dfa15c3348b4fc9bb8b062
ChakkerAmine <chakkeramine18@gmail.com>
2022-07-18 17:43:49 +0100

    Adding Poppins font

--- wp-content/themes/learning_wordpress/functions.php
+++ wp-content/themes/learning_wordpress/functions.php
@@ -2,6 +2,11 @@
 
 function theme_files(){
     wp_enqueue_style('main style',get_stylesheet_uri());
+    wp_enqueue_style('font1','https://fonts.googleapis.com');
+    wp_enqueue_style('font2','https://fonts.gstatic.com');
+    wp_enqueue_style('font3','https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;500&display=swap');
+    
+
 }
 add_action('wp_enqueue_scripts','theme_files');
 
