ce38680d9768f83a43e8934a1eff5abcfae71014
ChakkerAmine <chakkeramine18@gmail.com>
2022-07-29 12:43:23 +0100

    Adding icon for the team custom post on menu

--- wp-content/mu-plugins/cleaning-post-types.php
+++ wp-content/mu-plugins/cleaning-post-types.php
@@ -23,6 +23,32 @@ function cleaning_post_types(){
         'menu_icon' => 'dashicons-plus-alt2'
     );
     register_post_type('project',$args);
+
+
+
+    // Team Custom Post type 
+    $args   = array(
+        'supports' => array('title','editor','thumbnail'),
+        'public' => true,
+        'has_archive' => true,
+        'labels'=> array(
+            'name' => 'Teams',
+            'singular_name' => 'team',
+            'add_new' => 'Add New Member',
+            'add_new_item' => 'Add New Member',
+            'edit_item' => 'Edit Member',
+            'new_item' => 'New Member',
+            'all_items' => 'All Member',
+            'view_item' => 'View Member',
+            'search_items' => 'Search for a Member',
+            'not_found' =>  'No one Found',
+            'not_found_in_trash' => 'No one found in Trash', 
+            'parent_item_colon' => '',
+            'menu_name' => 'Our Team',
+        ),
+        'menu_icon' => 'dashicons-admin-users'
+    );
+    register_post_type('team',$args);
 }
 add_action('init','cleaning_post_types');
 
